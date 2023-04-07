644a3e70ed6a37a96c91ce41f069094283e65a3d
AlexAlkhatib <alexkh0507@gmail.com>
2023-04-07 23:17:49 +0200

    commentaires

--- app/src/main/java/com/mniprince/quiz/ProgressBarAnimation.java
+++ app/src/main/java/com/mniprince/quiz/ProgressBarAnimation.java
@@ -7,6 +7,10 @@ import android.view.animation.Transformation;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+/*
+La classe ProgressBarAnimation étend la classe Animation et est utilisée pour créer une animation de la barre de progression et de son texte correspondant.
+Cette classe a un constructeur qui prend en paramètres le contexte de l'application, la barre de progression, le texte de la barre de progression, la valeur de départ et la valeur finale de la barre de progression.
+ */
 public class ProgressBarAnimation extends Animation {
     private Context context;
     private ProgressBar progressBar;
@@ -22,6 +26,14 @@ public class ProgressBarAnimation extends Animation {
         this.to = to;
     }
 
+    /*
+    La classe ProgressBarAnimation hérite de la classe Animation et remplace la méthode applyTransformation pour effectuer une animation de la barre de progression.
+    La méthode prend en paramètres le temps d'interpolation et l'objet Transformation, qui est utilisé pour appliquer les transformations à la vue.
+    Elle calcule la valeur de la barre de progression et la met à jour à chaque étape de l'animation en utilisant la formule linéaire.
+    La valeur de la barre de progression est définie en pourcentage et est également affichée dans un TextView.
+    Si la valeur de la barre de progression atteint sa valeur maximale (correspondant à la valeur "to" définie dans le constructeur),
+    la méthode démarre une nouvelle activité CategoryActivity à l'aide d'un objet Intent.
+     */
     @Override
     protected void applyTransformation(float interpolatedTime, Transformation t) {
         super.applyTransformation(interpolatedTime, t);
