6108edb1599675e0a1334d9ca031e075cd40d5c0
AlexAlkhatib <alexkh0507@gmail.com>
2023-04-07 23:24:32 +0200

    Update RequestInteface.java

--- app/src/main/java/com/mniprince/quiz/RequestInteface.java
+++ app/src/main/java/com/mniprince/quiz/RequestInteface.java
@@ -5,6 +5,13 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+/*
+Cette classe est une interface pour définir les requêtes HTTP à envoyer à une API REST pour récupérer des données JSON. Elle utilise Retrofit, une bibliothèque Java pour simplifier l'appel des API REST.
+
+La méthode getJson() utilise l'annotation @GET pour spécifier la méthode HTTP (dans ce cas, GET) et l'URL de base de l'API. Elle prend également trois paramètres de requête : categories, limit et difficulty. Ces paramètres sont définis avec l'annotation @Query.
+
+La méthode renvoie un objet Call qui représente une demande asynchrone pour récupérer une liste d'objets QuizModel à partir de l'API.
+ */
 
 interface RequestInteface {
     @GET("api/questions?")
